86e52b32a008a6e51391b6b04f2fcd6b061e8894
sibcgh <sibghat.shah01@gmail.com>
2020-02-29 20:11:37 -0500

    created an simple API call with fetch

--- api/api.py
+++ api/api.py
@@ -7,3 +7,7 @@ app = Flask(__name__)
 def get_current_time():
         return {'time': time.time()}
     
+@app.route('/', methods=['GET'])
+def home():
+    return '''<h1>Distant Reading Archive</h1>
+<p>A prototype API for getting data about SciFi books.</p>'''
