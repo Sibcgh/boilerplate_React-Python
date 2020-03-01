3594e000800abdefe7dd25233bfe61a6f9908421
sibcgh <sibghat.shah01@gmail.com>
2020-02-29 20:25:38 -0500

    reformat of datetime

--- api/api.py
+++ api/api.py
@@ -1,13 +1,9 @@
 import time
+import datetime
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/time')
 def get_current_time():
-        return {'time': time.time()}
-    
-@app.route('/', methods=['GET'])
-def home():
-    return '''<h1>Distant Reading Archive</h1>
-<p>A prototype API for getting data about SciFi books.</p>'''
+        return {'time': datetime.datetime.now()}
